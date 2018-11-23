7e41c248e63d784868fd9158400fcb25d7457a52
resticDOG <1096340650@qq.com>
2018-11-23 20:05:02 +0800

    完成支付模块

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -56,4 +56,66 @@ public class Const {
         }
     }
 
+    /**
+     * 订单状态枚举类
+     */
+    public enum OrderStatusEnum{
+        CANCELED(0, "已取消"),
+        NO_PAY(10, "未支付"),
+        PAYD(20, "已付款"),
+        SHIPPED(40, "已发货"),
+        ORDER_SUCCESS(50, "订单完成"),
+        ORDER_CLOSED(60, "订单关闭");
+
+        int code;
+        String value;
+
+        OrderStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    /**
+     * 回调状态接口常量类
+     */
+    public interface AlipayCallback{
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+        //返回支付宝的字符串
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+    }
+
+    /**
+     * 支付平台枚举类
+     */
+    public enum PayPlatform{
+        ALIPAY(1, "支付宝");
+
+        int code;
+        String value;
+
+        PayPlatform(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
 }
